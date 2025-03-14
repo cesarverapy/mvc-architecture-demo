0802c9cc30a1db21b24d212706108eff79dabe35
cesarverapy <cesar.vera.dev@gmail.com>
2025-03-13 23:56:45 -0300

    set up basic request route

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send("welcome to the future e-commerce");
+});
+
 app.listen(PORT, (error) =>{
     if(!error)
         console.log("server running on port "+ PORT)
